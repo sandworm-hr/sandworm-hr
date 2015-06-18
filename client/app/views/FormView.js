257861075a3075db6ba622fadbf854fa417714b0
Edwin Lin <edwinlin1987@gmail.com>
2015-06-18 12:02:39 -0700

    Create basic formView

--- client/app/views/FormView.js
+++ client/app/views/FormView.js
@@ -1,13 +1,18 @@
 // Backbone view for the stock submission form
 var FormView = Backbone.View.extend({
 
-  el: 'div',
-
   initialize: function(){
+    this.render();
   },
 
   render: function(){
-    return this.$el;
+    return this.$el.html('\
+      <form>\
+        <input><span>Stock Symbol:</span>\
+        <input/><span>Date:</span>\
+        <input/><span>Amount ($)</span>\
+        <input type="submit"/>\
+      </form>');
   }
 
 });
