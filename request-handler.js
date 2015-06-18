22829304c64514b4c34b3626dc75b430774a8616
Paul Sokolik <paul.sokolik@gmail.com>
2015-06-18 12:31:47 -0700

    add initial yahoo finance api call; install yahoo npm module; requre modules

--- request-handler.js
+++ request-handler.js
@@ -1,1 +1,23 @@
 //Adding request handler file
+
+var yahooFinance = require('yahoo-finance');
+
+var getStocks = function(/* req,res 
+  */) {
+  // var ticker = req.body.symbol;
+  // var start = req.body.startDate;
+  // var end = req.body.endDate;
+  yahooFinance.historical({
+    symbol: 'AAPL',//ticker,
+    from: '2015-01-02',//start, //FORMAT: 'YYYY-MM-DD',
+    to: '2015-01-05',//end, //FORMAT: 'YYYY-MM-DD',
+    period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only) 
+  }, function (err, quotes) {
+    if(err) return console.log(err);
+    console.log(quotes);
+    // res.json(quotes);
+  });
+
+};
+
+module.exports.getStocks = getStocks;
