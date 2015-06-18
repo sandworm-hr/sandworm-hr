af11e994521e6b236a936336665846834c0d4c03
Edwin Lin <edwinlin1987@gmail.com>
2015-06-18 16:18:42 -0700

    Removed anonymous function from event

--- client/app/views/FormView.js
+++ client/app/views/FormView.js
@@ -3,25 +3,32 @@ var FormView = Backbone.View.extend({
   
   initialize: function(){
     this.render();
-    this.stopSpinner();
-    this.collection.on('sync', this.stopSpinner, this);
+    //stop loading spinner on page load
+    this.stopSpinner(); 
+    //stop spinner upon request completion
+    this.collection.on('sync', this.stopSpinner, this); 
   },
 
   events: {
-    'submit': function(e) {
-      e.preventDefault();
-      this.startSpinner();
-      var d = new Date();
-      this.collection.create({
-        symbol: this.$('#symbol').val(),
-        from: this.$('#date').val(),
-        amount: this.$('#amount').val(),
-        to: d.toISOString().slice(0,10),
-        period: 'd'
-      });
-    }
+    //Form submission form
+    'submit': 'handleSubmit'
   },
 
+  handleSubmit: function(e) {
+    e.preventDefault();
+    //start spinner upon stock creation
+    this.startSpinner(); 
+    var d = new Date();
+    /* Create will create a new stock in the collection
+       and send a request for the pertinent information */
+    this.collection.create({
+      symbol: this.$('#symbol').val(),
+      from: this.$('#date').val(),
+      amount: this.$('#amount').val(),
+      to: d.toISOString().slice(0,10), //Just the YYYY-MM-DD portion
+    });
+  },
+  
   startSpinner: function(){
     this.$('img').show();
   },
@@ -32,6 +39,7 @@ var FormView = Backbone.View.extend({
 
 
   render: function(){
+    //Render main form
     return this.$el.html('\
       <form>\
         <input type="text" id="symbol"><span>Stock Symbol:</span>\
