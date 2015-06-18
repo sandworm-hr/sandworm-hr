d1f7d2323fd23288857eb7ef87717b1e2217dabe
Edwin Lin <edwinlin1987@gmail.com>
2015-06-17 18:38:02 -0700

    Create barebones server file with express

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client'));
+
+
+
+
+
+var port = 8080;
+
+app.listen(port);
