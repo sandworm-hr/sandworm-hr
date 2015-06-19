fe05f1c7dc61a8ccba579e8444ebdc5da1e3d2c7
Paul Sokolik <paul.sokolik@gmail.com>
2015-06-19 16:33:56 -0700

    add working graph view d3 functionality

--- client/app/views/GraphView.js
+++ client/app/views/GraphView.js
@@ -1,29 +1,38 @@
 // Backbone view for the graph
 var GraphView = Backbone.View.extend({
 
+  className: 'graph',
+
   initialize: function() {
     this.render();
+    this.collection.on('sync', function() {
+      this.render();
+    }, this);
   },
 
   plotLine: function(stock) {
     var Xrange = [0,500];//stock.getDateRange();
-    var Yrange = stock.getValRange();
+    var Yrange = [500, 0];
     var lineData = stock.getTrajectory();
     
     var x = d3.time.scale().range(Xrange);
-    var y = d3.linear.scale().range(Yrange);
+    x.domain(d3.extent(lineData, function(d) {return d.date; }));
+    
+    var y = d3.scale.linear().range(Yrange);
+    y.domain(d3.extent(lineData, function(d) {return d.value; }));
 
     var lineFunction = d3.svg.line()
-        .x(function(d) { return d.date; })
-        .y(function(d) { return d.value; });
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.value); })
+        .interpolate("linear");
 
-    var svg = d3.select($el).append("svg")
-        .attr("width", Xrange[1] - Xrange[0])//width + margin.left + margin.right)
-        .attr("height", Yrange[1] - Yrange[0])//height + margin.top + margin.bottom)
-        .append("g");
+    var svg = d3.select('.graph').append("svg")
+        .attr("width", 500)//width + margin.left + margin.right)
+        .attr("height", 500);//height + margin.top + margin.bottom)
+        //.append("g");
         // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var lineGraph = svg.append("g")
+    var lineGraph = svg.append("path")
         .attr("d", lineFunction(lineData))
         .attr("stroke", "blue")
         .attr("stroke-width", 2)
