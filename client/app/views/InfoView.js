eadbd682449e02c23370bb7fcc1084d3516ec92e
Edwin Lin <edwinlin1987@gmail.com>
2015-06-22 14:29:39 -0700

    Modified StockView and InfoView to reflect the price changes

--- client/app/views/InfoView.js
+++ client/app/views/InfoView.js
@@ -3,17 +3,30 @@ var InfoView = Backbone.View.extend({
 
   className: 'info',
 
+  template: _.template('<div>\
+    <span> Portfolio Initial Value : <%= start %></span>\
+    <span> Portfolio Final Value : <%= end %></span>\
+    <span>Change: <% if (percentage >= 0) { %> UP <% } else { %> DOWN <% } %> <%= Math.abs(percentage) %>%</span>\
+    </div> '),
+
   initialize: function() {
-    this.collection.on('add remove', this.render, this)
+    this.collection.on('sync', this.render, this);
   },
 
   render: function() {
     this.$el.children().detach();
+    var port = {};
+    port.start = 0;
+    port.end = 0;
     this.$el.append(
       this.collection.map(function(item) {
+        port.start += item.get('amount');
+        port.end += item.getEndVal();
         return new StockView({model: item}).render();
       })
     );
+    port.percentage = Math.round((port.end/port.start - 1) * 100);
+    this.$el.append(this.template(port));
   }
 
 });
