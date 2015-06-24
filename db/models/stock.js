7d98f93c54e13fb479bd41691af13271910daf96
Edwin Lin <edwinlin1987@gmail.com>
2015-06-24 11:12:48 -0700

    Add models/collections for database

---
+++ db/models/stock.js
@@ -0,0 +1,11 @@
+var db = require('../config');
+
+var Stock = db.Model.extend({
+  tableName: 'stocks',
+  hasTimestamps: true,
+  portfolio: function() {
+    return this.belongsTo(Portfolio, 'portfolio_id');
+  },
+});
+
+module.exports = Score;
