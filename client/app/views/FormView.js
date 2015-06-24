c54e6dc98ed1884431bc607c3ebb32c2cee99f1b
Tamara <tmwoodson@gmail.com>
2015-06-23 18:46:17 -0700

    Start splitting FormView into subparts

--- client/app/views/FormView.js
+++ client/app/views/FormView.js
@@ -6,7 +6,7 @@ var FormView = Backbone.View.extend({
   divText: '\
       <div class="container"> \
         <div class="row"> \
-          <div class="col-md-6 col-md-offset-3 well">\
+          <div class="col-md-6 col-md-offset-3 well text-center">\
             <form role="form">\
               <div class="form-group"> \
                 <label for="symbol">Stock Symbol</label>\
@@ -17,15 +17,21 @@ var FormView = Backbone.View.extend({
                 <input type="date" id="date" class="form-control">\
               </div> \
               <div class="form-group"> \
-                 <label for="date">Amount</label>\
-                $<input type="text" id="amount" class="form-control">\
+                 <label for="date">Amount ($)</label>\
+                 <input type="text" id="amount" class="form-control">\
               </div> \
               <button type="submit" class="btn btn-default">Submit</button>\
+              <img src="assets/images/loader.gif">\
             </form>\
           </div> \
         </div> \
       </div>',
-  
+
+  pickStockEl: '<div class="form-group text-center">\
+                  <label for="symbol">Pick a Stock</label>\
+                  <input type="text" id="symbol" class="form-control">\
+                </div>',
+
   initialize: function(){
     this.render();
     //stop loading spinner on page load
