6eb674e86028030fd369acf56e65d839ab87aa02
Edwin Lin <edwinlin1987@gmail.com>
2015-06-26 14:11:59 -0700

    Add old portfolios display

--- client/app/views/PortfolioView.js
+++ client/app/views/PortfolioView.js
@@ -24,6 +24,7 @@ var PortfolioView = Backbone.View.extend({
       type: 'POST',
       data: {'id': id},
       success: function(data) {
+        console.log(data);
         data.forEach(function (stock) {
           stocks.create(stock);
         });
